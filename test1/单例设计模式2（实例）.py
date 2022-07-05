8adf27ae7246473de8eb9c909a55e92608a255e3
xiaobuliu0757 <751097263@qq.com>
2022-07-05 23:49:20 +0800

    单例设计模式2实例

--- test1/单例设计模式2（实例）.py
+++ test1/单例设计模式2（实例）.py
@@ -2,6 +2,8 @@ class MusicPlay(object):
     # 记录第一个被创建对象的引用
     instance = None
 
+    #记录是否执行过初始化动作
+    init_flag = False
     def __new__(cls, *args, **kwargs):
         # 1、判断类属性是否是空对象
         if cls.instance is None:
@@ -11,6 +13,11 @@ class MusicPlay(object):
         # 返回类属性保存的对象引用
         return cls.instance
 
+    def __init__(self):
+        if MusicPlay.init_flag:
+            return
+        print("初始化播放器")
+        MusicPlay.init_flag = True
 yyy = MusicPlay()
 qqmusic = MusicPlay()
 print(yyy)
