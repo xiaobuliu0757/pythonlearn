d4689f377b9a2bdc8296eafa6f4304ee0097dcb3
xiaobuliu0757 <751097263@qq.com>
2022-07-22 23:19:33 +0800

    提取json字段输出到文本文档3.0

--- tool/min_choose_option.py
+++ tool/min_choose_option.py
@@ -9,15 +9,29 @@ question_code = jsonpath(file_json, "$..question_code")
 min_choose_option = jsonpath(file_json, "$..min_choose_option")
 #提供个列表待会存数据
 list = []
+not_answer = []
+should_answer = []
 # 利用迭代方式实现A\B两两凑对
 for x,y in zip(question_code,min_choose_option):
     # 转成字符串给writeline用
     list = str([x,y])
-    # 打开文本文档，a是指在末尾连接输出
-    Note = open('result.txt', mode='a')
+    # 判断是否必填：0非必填，1必填
+    if y:
+        not_answer.append(x)
+        # print("非必填： %s" % not_answer)
+    else:
+        should_answer.append(x)
+        # print("必填： %s" % should_answer)
+    # 打开文本文档，w是指在覆盖上一份文件
+    Note = open('result.txt', mode='w')
     # 通过“+”号加了个换行好看点
-    Note.writelines(list+'\n')
-print("结果已更新到'result.txt'文件中~")
-# 记得关下文档哦
+    Note.writelines("非必填：%s"% not_answer+'\n')
+    Note.writelines("必填：%s"% should_answer)
+# 记得关下文档
 Note.close()
+print("运行成功，结果已更新到'result.txt'文件中~")
+
+
+
+
 
