461de40687c4adbacc255fa3cc559d7666c568f8
xiaobuliu0757 <751097263@qq.com>
2022-07-23 09:07:11 +0800

    增加逻辑判断是否必填

--- tool/min_choose_option.py
+++ tool/min_choose_option.py
@@ -3,11 +3,10 @@ import json
 # 打开存放json格式的数据
 with open('./origin.txt', 'r', encoding='utf-8') as file:
     file_json = json.loads(file.readline())
-
 # 提取想要的字段
 question_code = jsonpath(file_json, "$..question_code")
 min_choose_option = jsonpath(file_json, "$..min_choose_option")
-#提供个列表待会存数据
+#提供个列表待会存数据用
 list = []
 not_answer = []
 should_answer = []
@@ -15,13 +14,11 @@ should_answer = []
 for x,y in zip(question_code,min_choose_option):
     # 转成字符串给writeline用
     list = str([x,y])
-    # 判断是否必填：0非必填，1必填
+    # 0非必填，1必填
     if y:
-        not_answer.append(x)
-        # print("非必填： %s" % not_answer)
-    else:
         should_answer.append(x)
-        # print("必填： %s" % should_answer)
+    else:
+        not_answer.append(x)
     # 打开文本文档，w是指在覆盖上一份文件
     Note = open('result.txt', mode='w')
     # 通过“+”号加了个换行好看点
